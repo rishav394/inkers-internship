cfe8fc2659c54c591b9578093b7ea472979308c2
Rishav Rungta <rishav394@gmail.com>
2020-02-27 23:37:36 +0530

    Fix design heavily. Responsive now. HR probably gonna open in mobile :P

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const i18n = require('i18n');
 const path = require('path');
 const passport = require('./config/passport-setup');
 const authRoute = require('./routes/auth-routes');
 const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 const mongodb = require('./mongodb/mongodb.connect');
 const uploadRoute = require('./routes/upload-routes');
 const allUploads = require('./lib/upload-finder');
@@ -17,13 +19,22 @@ app.use(
 		keys: [process.env.SECRET],
 	}),
 );
+app.use(cookieParser());
+i18n.configure({
+	locales: ['en', 'hi', 'bn', 'ta', 'kn'],
 
+	// sets a custom cookie name to parse locale settings from
+	cookie: 'locale',
+	defaultLocale: 'en',
+	// where to store json files - defaults to './locales'
+	directory: __dirname + '/locales',
+});
+app.use(i18n.init);
 mongodb.connect(process.env.dbURI);
 
 // init passport
 passport(app);
 
-app.use(express.static('public'));
 app.use(express.json());
 app.use(
 	express.urlencoded({
@@ -33,6 +44,7 @@ app.use(
 app.use('/auth', authRoute);
 app.use('/upload', uploadRoute);
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
